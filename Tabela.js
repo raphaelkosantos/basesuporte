3bcca32db10640e45c9f050523dc31b76c695bb7
Raphael Santos <106029714+raphaelkosantos@users.noreply.github.com>
2023-05-06 19:09:10 -0300

    Add files via upload

---
+++ Tabela.js
@@ -0,0 +1,18 @@
+
+
+<script>
+
+document.getElementById("btn6").addEventListener("click", tabela);
+
+
+funtion tabela(){
+
+google.script.run.withSuccessHandler(GerarTabela).DadosTabela();
+
+
+
+}
+
+
+
+</script>
